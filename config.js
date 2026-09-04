b22b15cc82093260ecf47b277cd3a090901f488f
sd-karuwisi2 <sittirahma857@gmail.com>
2026-09-04 20:09:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // Nanti, ganti 'URL_WEB_APP_ANDA_DISINI' dengan URL asli yang didapat dari Google Apps Script.
 
 const CONFIG = {
-    GOOGLE_SHEETS_API_URL: "https://script.google.com/macros/s/AKfycbyM3nzWbBtbabCfE6-8p11iuFSupoM5afTYjUzSVgzalpwM6FwcwhWOul7JdOeSC8uYew/exec" 
+    GOOGLE_SHEETS_API_URL: "https://script.google.com/macros/s/AKfycbzK4pbM-wfEhbTAr3Mrm1e3V-5xg91gsWBCAl0zkR1P6Mzg4hFv7nBs20TVhtJR9yWs/exec" 
 };
